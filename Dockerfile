488ec1b047fba0839fd2d6c55adc6d13a314b48c
Alex Goodman <wagoodman@gmail.com>
2018-05-20 10:39:59 -0400

    add filetree

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
 FROM alpine:latest
 ADD README.md /somefile.txt
-RUN cp /somefile.txt /root/somefile.txt
+RUN mkdir /root/example
+RUN cp /somefile.txt /root/example/somefile1.txt
+RUN cp /somefile.txt /root/example/somefile2.txt
+RUN cp /somefile.txt /root/example/somefile3.txt
+RUN mv /root/example/somefile3.txt /root/saved.txt
+RUN rm -rf /root/example/
--- tree.go
+++ tree.go
@@ -1,14 +1,21 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"sort"
+)
 
-type Entity interface {
-	Visit(Visiter)
-}
+const (
+	newLine      = "\n"
+	emptySpace   = "    "
+	middleItem   = "├── "
+	continueItem = "│   "
+	lastItem     = "└── "
+)
 
 type Tree struct {
 	root Node
-	size uint
+	size int
 }
 
 type Node struct {
@@ -19,21 +26,23 @@ type Node struct {
 	children map[string]*Node
 }
 
-type Visiter func(*Node)
-
 func NewTree() (tree *Tree) {
 	tree = new(Tree)
-	tree.size = 1
+	tree.size = 0
 	root := &tree.root
 	root.tree = tree
+	root.name = ""
 	root.children = make(map[string]*Node)
 	return tree
 }
 
-func NewNode(name string, data interface{}) (node *Node) {
+func NewNode(parent *Node, name string, data interface{}) (node *Node) {
+	node = new(Node)
 	node.name = name
 	node.data = data
 	node.children = make(map[string]*Node)
+	node.parent = parent
+	node.tree = parent.tree
 	return node
 }
 
@@ -41,29 +50,69 @@ func (tree *Tree) Root() *Node {
 	return &tree.root
 }
 
-func (tree *Tree) Visit(visiter Visiter) {
-	tree.root.Visit(visiter)
-}
-
-func (parent *Node) Add(name string, data interface{}) (child *Node, error error) {
-	if parent.children[name] != nil {
-		return nil, errors.New("Duplicate child")
+func (node *Node) AddChild(name string, data interface{}) (child *Node, error error) {
+	if node.children[name] != nil {
+		return nil, errors.New("Tree node already exists")
 	}
-	child = NewNode(name, data)
-	child.tree = parent.tree
-	child.parent = parent
-	child.tree.size++
-	parent.children[name] = child
+	child = NewNode(node, name, data)
+	node.children[name] = child
+	node.tree.size++
 	return child, nil
 }
 
-func (node *Node) Visit(visiter Visiter) {
+func (node *Node) Remove() error {
 	for _, child := range node.children {
-		child.Visit(visiter)
+		child.Remove()
 	}
-	visiter(node)
+	delete(node.parent.children, node.name)
+	node.tree.size--
+	return nil
 }
 
-func main() {
+func (node *Node) String() string {
+	return node.name
+}
 
+func (tree *Tree) String() string {
+	var renderLine func(string, []bool, bool) string
+	var walkTree func(*Node, []bool) string
+
+	renderLine = func(text string, spaces []bool, last bool) string {
+		var result string
+		for _, space := range spaces {
+			if space {
+				result += emptySpace
+			} else {
+				result += continueItem
+			}
+		}
+
+		indicator := middleItem
+		if last {
+			indicator = lastItem
+		}
+
+		return result + indicator + text + newLine
+	}
+
+	walkTree = func(node *Node, spaces []bool) string {
+		var result string
+		var keys []string
+		for key := range node.children {
+			keys = append(keys, key)
+		}
+		sort.Strings(keys)
+		for idx, name := range keys {
+			child := node.children[name]
+			last := idx == (len(node.children) - 1)
+			result += renderLine(child.String(), spaces, last)
+			if len(child.children) > 0 {
+				spacesChild := append(spaces, last)
+				result += walkTree(child, spacesChild)
+			}
+		}
+		return result
+	}
+
+	return "." + newLine + walkTree(tree.Root(), []bool{})
 }
