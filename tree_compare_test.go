07550706d732433ee3779480ece5017094b82cd9
Will Murphy <willmurphyscode@gmail.com>
2018-05-28 09:59:01 -0400

    test written

--- tree_compare_test.go
+++ tree_compare_test.go
@@ -1,1 +1,34 @@
 package main
+
+import "testing"
+
+func compareToTest(t *testing.T) {
+	lowerTree := NewTree()
+	upperTree := NewTree()
+	paths := [5]string{"/etc", "/etc/sudoers", "/etc/hosts", "/usr/bin", "/usr/bin/bash"}
+	var zeros = [16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+	for _, value := range paths {
+		fakeData := FileChangeInfo{
+			path:     value,
+			typeflag: 1,
+			md5sum:   zeros,
+			diffType: nil,
+		}
+
+		lowerTree.AddPath(value, fakeData)
+		upperTree.AddPath(value, fakeData)
+	}
+	lowerTree.compareTo(upperTree)
+	asserter := func(n *Node) error {
+		data := n.data.(FileChangeInfo)
+		if data.diffType == nil {
+			t.Errorf("Expected node at %s to have DiffType unchanged, but had nil", n.Path())
+		}
+		if *data.diffType != Unchanged {
+			t.Errorf("Expecting node at %s to have DiffType unchanged, but had %v", n.Path(), *data.diffType)
+		}
+		return nil
+	}
+	lowerTree.Visit(asserter)
+}
