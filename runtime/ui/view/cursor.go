a885fa6e68b3763d52de20603ee1b9cd8949276f
Luka Markušić <markusicluka@gmail.com>
2023-09-06 21:29:07 +0200

    Fix scrolling after gocui update
    Revert

--- runtime/ui/view/cursor.go
+++ runtime/ui/view/cursor.go
@@ -1,38 +1,17 @@
 package view
 
 import (
-	"errors"
-
 	"github.com/awesome-gocui/gocui"
 )
 
 // CursorDown moves the cursor down in the currently selected gocui pane, scrolling the screen as needed.
-func CursorDown(g *gocui.Gui, v *gocui.View) error {
-	return CursorStep(g, v, 1)
+func CursorDown(v *gocui.View, step uint) error {
+	v.MoveCursor(0, int(step))
+	return nil
 }
 
 // CursorUp moves the cursor up in the currently selected gocui pane, scrolling the screen as needed.
-func CursorUp(g *gocui.Gui, v *gocui.View) error {
-	return CursorStep(g, v, -1)
-}
-
-// Moves the cursor the given step distance, setting the origin to the new cursor line
-func CursorStep(g *gocui.Gui, v *gocui.View, step int) error {
-	cx, cy := v.Cursor()
-
-	// if there isn't a next line
-	line, err := v.Line(cy + step)
-	if err != nil {
-		return err
-	}
-	if len(line) == 0 {
-		return errors.New("unable to move the cursor, empty line")
-	}
-	if err := v.SetCursor(cx, cy+step); err != nil {
-		ox, oy := v.Origin()
-		if err := v.SetOrigin(ox, oy+step); err != nil {
-			return err
-		}
-	}
+func CursorUp(v *gocui.View, step uint) error {
+	v.MoveCursor(0, int(-step))
 	return nil
 }
