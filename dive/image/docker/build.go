b15db1744b92b77e29e2cdaaf6e58b7d8582ca5a
artmb <artem.burashnikov@gmail.com>
2025-03-30 19:19:32 +0300

    Add lowercase logic to Containerfile and Dockerfile discovery function
    This commit adds:
    * Lowercase test for Containerfile discovery function.
      It is now possible to specify `containerfile` or `dockerfile` path
      or discover them in a build context.
    * Build arguments `--file` flag test.

--- dive/image/docker/build.go
+++ dive/image/docker/build.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+)
+
+const (
+	defaultDockerfileName    = "Dockerfile"
+	defaultContainerfileName = "Containerfile"
 )
 
 func buildImageFromCli(buildArgs []string) (string, error) {
@@ -14,12 +20,17 @@ func buildImageFromCli(buildArgs []string) (string, error) {
 	defer os.Remove(iidfile.Name())
 	defer iidfile.Close()
 
-	containerFilePath, err := tryFindContainerFile(buildArgs)
-	if err != nil {
-		return "", err
+	var allArgs []string
+	if isFileFlagsAreSet(buildArgs, "-f", "--file") {
+		allArgs = append([]string{"--iidfile", iidfile.Name()}, buildArgs...)
+	} else {
+		containerFilePath, err := tryFindContainerfile(buildArgs)
+		if err != nil {
+			return "", err
+		}
+		allArgs = append([]string{"--iidfile", iidfile.Name(), "-f", containerFilePath}, buildArgs...)
 	}
 
-	allArgs := append([]string{"--iidfile", iidfile.Name(), "-f", containerFilePath}, buildArgs...)
 	err = runDockerCmd("build", allArgs...)
 	if err != nil {
 		return "", err
@@ -33,36 +44,42 @@ func buildImageFromCli(buildArgs []string) (string, error) {
 	return string(imageId), nil
 }
 
-// This functions loops through a provided build arguments and tries to find a Containerfile or a Dockerfile.
-func tryFindContainerFile(buildArgs []string) (string, error) {
-	var fileFlag bool
-	var filePath string
-
-	// If the `-f` flag was set assume the correct path was provided and use it...
-	for i, arg := range buildArgs {
-		if arg == "-f" && i+1 < len(buildArgs) {
-			fileFlag = true
-			filePath = buildArgs[i+1]
-			break
+// Checks if specified flags are present in the arguments list.
+func isFileFlagsAreSet(args []string, flags ...string) bool {
+	for i, arg := range args {
+		for _, flag := range flags {
+			if arg == flag && i+1 < len(args) {
+				return true
+			}
 		}
 	}
+	return false
+}
 
-	if fileFlag {
-		return filePath, nil
-	}
-
-	// ... otherwise we have to look for a build context within the provided build arguments.
-	// Test build arguments one by one to find a valid path containing `Containerfile` or a `Dockerfile` (in that order).
+// This functions loops through a provided build arguments and tries to find a Containerfile or a Dockerfile.
+func tryFindContainerfile(buildArgs []string) (string, error) {
+	// Look for a build context within the provided build arguments.
+	// Test build arguments one by one to find a valid path containing default names of `Containerfile` or a `Dockerfile` (in that order).
 	for _, arg := range buildArgs {
 		if fileInfo, err := os.Stat(arg); err == nil && fileInfo.IsDir() {
-			containerfilePath := filepath.Join(arg, "Containerfile")
-			dockerfilePath := filepath.Join(arg, "Dockerfile")
-
+			containerfilePath := filepath.Join(arg, defaultContainerfileName)
 			if _, err := os.Stat(containerfilePath); err == nil {
 				return containerfilePath, nil
 			}
+
+			altContainerfilePath := filepath.Join(arg, strings.ToLower(defaultContainerfileName))
+			if _, err := os.Stat(altContainerfilePath); err == nil {
+				return altContainerfilePath, nil
+			}
+
+			dockerfilePath := filepath.Join(arg, defaultDockerfileName)
 			if _, err := os.Stat(dockerfilePath); err == nil {
 				return dockerfilePath, nil
+			}
+
+			altDockerfilePath := filepath.Join(arg, strings.ToLower(defaultDockerfileName))
+			if _, err := os.Stat(altDockerfilePath); err == nil {
+				return altDockerfilePath, nil
 			}
 		}
 	}
