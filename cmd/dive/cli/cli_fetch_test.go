ab8670bc957bb69dffa426bb842e46a0c90a1772
Alex Goodman <wagoodman@users.noreply.github.com>
2025-04-08 11:35:22 -0400

    add cli tests for source flag

--- cmd/dive/cli/cli_fetch_test.go
+++ cmd/dive/cli/cli_fetch_test.go
@@ -8,42 +8,65 @@ import (
 	"testing"
 )
 
-func Test_FetchImage(t *testing.T) {
+func Test_LoadImage(t *testing.T) {
+	image := "busybox:1.37.0@sha256:ad9fa4d07136a83e69a54ef00102f579d04eba431932de3b0f098cc5d5948f9f"
+	archive := repoPath(t, ".data/test-docker-image.tar")
 
-	t.Run("fetch from docker engine", func(t *testing.T) {
-		rootCmd := getTestCommand(t, "docker://busybox:1.37.0@sha256:ad9fa4d07136a83e69a54ef00102f579d04eba431932de3b0f098cc5d5948f9f")
-		combined := Capture().WithStdout().WithStderr().Run(t, func() {
-			require.NoError(t, rootCmd.Execute())
-		})
-
-		assert.Contains(t, combined, "Loading image")
-		assert.Contains(t, combined, "Analyzing image")
-		assert.Contains(t, combined, "Evaluating image")
-
-		snaps.MatchSnapshot(t, combined)
+	t.Run("from docker engine", func(t *testing.T) {
+		runWithCombinedOutput(t, fmt.Sprintf("docker://%s", image))
 	})
 
-	t.Run("fetch from podman engine", func(t *testing.T) {
+	t.Run("from docker engine (flag)", func(t *testing.T) {
+
+		runWithCombinedOutput(t, fmt.Sprintf("--source docker %s", image))
+	})
+
+	t.Run("from podman engine", func(t *testing.T) {
+		if _, err := exec.LookPath("podman"); err != nil {
+			t.Skip("podman not installed, skipping test")
+		}
+		// pull the image from podman first
+		require.NoError(t, exec.Command("podman", "pull", image).Run())
+
+		runWithCombinedOutput(t, fmt.Sprintf("podman://%s", image))
+	})
+
+	t.Run("from podman engine (flag)", func(t *testing.T) {
 		if _, err := exec.LookPath("podman"); err != nil {
 			t.Skip("podman not installed, skipping test")
 		}
 
-		image := "busybox:1.37.0@sha256:ad9fa4d07136a83e69a54ef00102f579d04eba431932de3b0f098cc5d5948f9f"
-
 		// pull the image from podman first
 		require.NoError(t, exec.Command("podman", "pull", image).Run())
 
-		rootCmd := getTestCommand(t, fmt.Sprintf("podman://%s", image))
-		combined := Capture().WithStdout().WithStderr().Run(t, func() {
-			require.NoError(t, rootCmd.Execute())
-		})
-
-		assert.Contains(t, combined, "Loading image")
-		assert.Contains(t, combined, "Analyzing image")
-		assert.Contains(t, combined, "Evaluating image")
-
-		snaps.MatchSnapshot(t, combined)
+		runWithCombinedOutput(t, fmt.Sprintf("--source podman %s", image))
 	})
+
+	t.Run("from archive", func(t *testing.T) {
+		runWithCombinedOutput(t, fmt.Sprintf("docker-archive://%s", archive))
+	})
+
+	t.Run("from archive (flag)", func(t *testing.T) {
+		runWithCombinedOutput(t, fmt.Sprintf("--source docker-archive %s", archive))
+	})
+}
+
+func runWithCombinedOutput(t testing.TB, cmd string) {
+	t.Helper()
+	rootCmd := getTestCommand(t, cmd)
+	combined := Capture().WithStdout().WithStderr().Run(t, func() {
+		require.NoError(t, rootCmd.Execute())
+	})
+
+	assertLoadOutput(t, combined)
+}
+
+func assertLoadOutput(t testing.TB, combined string) {
+	t.Helper()
+	assert.Contains(t, combined, "Loading image")
+	assert.Contains(t, combined, "Analyzing image")
+	assert.Contains(t, combined, "Evaluating image")
+	snaps.MatchSnapshot(t, combined)
 }
 
 func Test_FetchFailure(t *testing.T) {
--- cmd/dive/cli/internal/options/analysis.go
+++ cmd/dive/cli/internal/options/analysis.go
@@ -29,7 +29,7 @@ func DefaultAnalysis() Analysis {
 	return Analysis{
 		ContainerEngine:           defaultContainerEngine,
 		IgnoreErrors:              false,
-		AvailableContainerEngines: []string{"docker", "podman"},
+		AvailableContainerEngines: dive.ImageSources,
 	}
 }
 
